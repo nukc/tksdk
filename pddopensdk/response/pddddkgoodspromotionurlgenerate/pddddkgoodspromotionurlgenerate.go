adeba176af6bd80cd16658600f9f22406c4cc0dc
uxc <uxc@1006.com>
2022-09-19 17:02:20 +0800

    sync pdd.ddk.goods.promotion.url.generate response fields

--- pddopensdk/response/pddddkgoodspromotionurlgenerate/pddddkgoodspromotionurlgenerate.go
+++ pddopensdk/response/pddddkgoodspromotionurlgenerate/pddddkgoodspromotionurlgenerate.go
@@ -29,22 +29,36 @@ type GoodsPromotionURLGenerateResponse struct {
 }
 
 type GoodsPromotionURLList struct {
-	MobileURL            string                  `json:"mobile_url"`
-	WeAppInfo            GoodsPromotionWeAppInfo `json:"we_app_info"`
-	MobileShortURL       string                  `json:"mobile_short_url"`
-	WeAppWebViewURL      string                  `json:"we_app_web_view_url"`
-	URL                  string                  `json:"url"`
-	ShortURL             string                  `json:"short_url"`
-	WeAppWebViewShortURL string                  `json:"we_app_web_view_short_url"`
-	FFXOpenAppUrl        string                  `json:"ffx_open_app_url"`
+	MobileURL       string                  `json:"mobile_url"`
+	WeAppInfo       GoodsPromotionWeAppInfo `json:"we_app_info"`
+	MobileShortURL  string                  `json:"mobile_short_url"`
+	URL             string                  `json:"url"`
+	ShortURL        string                  `json:"short_url"`
+	SchemaUrl       string                  `json:"schema_url"`
+	TzSchemaUrl     string                  `json:"tz_schema_url"`
+	QQAppInfo       GoodsPromotionQQAppInfo `json:"qq_app_info"`
+	WeixinCode      string                  `json:"weixin_code"`
+	WeixinShortLink string                  `json:"weixin_short_link"`
 }
 
 type GoodsPromotionWeAppInfo struct {
 	WeAppIconURL      string `json:"we_app_icon_url"`
+	BannerUrl         string `json:"banner_url"`
 	UserName          string `json:"user_name"`
 	PagePath          string `json:"page_path"`
 	SourceDisplayName string `json:"source_display_name"`
 	Title             string `json:"title"`
 	AppID             string `json:"app_id"`
 	Desc              string `json:"desc"`
+}
+
+type GoodsPromotionQQAppInfo struct {
+	AppID             string `json:"app_id"`
+	BannerUrl         string `json:"banner_url"`
+	Desc              string `json:"desc"`
+	PagePath          string `json:"page_path"`
+	QQAppIconUrl      string `json:"qq_app_icon_url"`
+	SourceDisplayName string `json:"source_display_name"`
+	Title             string `json:"title"`
+	UserName          string `json:"user_name"`
 }
