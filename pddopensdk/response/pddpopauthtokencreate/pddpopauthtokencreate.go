724c47ed48290885891f124d4f3bd11fbb6b9287
uxc <uxc@1006.com>
2022-09-21 15:08:21 +0800

    update pdd token response model

--- pddopensdk/response/pddpopauthtokencreate/pddpopauthtokencreate.go
+++ pddopensdk/response/pddpopauthtokencreate/pddpopauthtokencreate.go
@@ -25,20 +25,20 @@ func (t *Response) WrapResult(result string) {
 
 type PopAuthTokenCreateResponse struct {
 	AccessToken           string   `json:"access_token"`
-	ExpiresAt             uint64   `json:"expires_at"`               // access_token过期时间点
+	ExpiresAt             int64    `json:"expires_at"`               // access_token过期时间点
 	ExpiresIn             int      `json:"expires_in"`               // access_token过期时间段，10（表示10秒后过期）
 	OwnerId               string   `json:"owner_id"`                 // 商家店铺id
 	OwnerName             string   `json:"owner_name"`               // 商家账号名称
-	R1ExpiresAt           uint64   `json:"r1_expires_at"`            // r1级别API或字段的访问过期时间点
+	R1ExpiresAt           int64    `json:"r1_expires_at"`            // r1级别API或字段的访问过期时间点
 	R1ExpiresIn           int      `json:"r1_expires_in"`            // r1级别API或字段的访问过期时间； 10（表示10秒后过期）
-	R2ExpiresAt           uint64   `json:"r2_expires_at"`            // r2级别API或字段的访问过期时间点
+	R2ExpiresAt           int64    `json:"r2_expires_at"`            // r2级别API或字段的访问过期时间点
 	R2ExpiresIn           int      `json:"r2_expires_in"`            // r2级别API或字段的访问过期时间；10（表示10秒后过期）
 	RefreshToken          string   `json:"refresh_token"`            // refresh token，可用来刷新access_token
-	RefreshTokenExpiresAt uint64   `json:"refresh_token_expires_at"` // Refresh token过期时间点
-	RefreshTokenExpiresIn uint64   `json:"refresh_token_expires_in"` // refresh_token过期时间段，10表示10秒后过期
+	RefreshTokenExpiresAt int64    `json:"refresh_token_expires_at"` // Refresh token过期时间点
+	RefreshTokenExpiresIn int64    `json:"refresh_token_expires_in"` // refresh_token过期时间段，10表示10秒后过期
 	Scope                 []string `json:"scope"`                    // 接口列表
-	W1ExpiresAt           uint64   `json:"w1_expires_at"`            // w1级别API或字段的访问过期时间点
+	W1ExpiresAt           int64    `json:"w1_expires_at"`            // w1级别API或字段的访问过期时间点
 	W1ExpiresIn           int      `json:"w1_expires_in"`            // w1级别API或字段的访问过期时间； 10（表示10秒后过期）
-	W2ExpiresAt           uint64   `json:"w2_expires_at"`            // w2级别API或字段的访问过期时间点
+	W2ExpiresAt           int64    `json:"w2_expires_at"`            // w2级别API或字段的访问过期时间点
 	W2ExpiresIn           int      `json:"w2_expires_in"`            // w2级别API或字段的访问过期时间；10（表示10秒后过期）
 }
